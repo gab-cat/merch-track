887092ac03b70773193956424446cb2c03c319e7
gab-cat <catimbanggabriel@gmail.com>
2024-05-17 02:14:11 +0800

    Update: Implemented tailwind to index

--- merchtrack/app/urls.py
+++ merchtrack/app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path("trackOrder", views.home, name='trackOrder'),
+    path("aboutUs", views.home, name='aboutUs'),
+    path("contactUs", views.home, name='contactUs'),
 ]
