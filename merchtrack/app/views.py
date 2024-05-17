df66aaf497c6eeebbc50a894ab755c819239823a
gab-cat <catimbanggabriel@gmail.com>
2024-05-18 00:42:02 +0800

    Update: Added Error warnings

--- merchtrack/app/views.py
+++ merchtrack/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import user_info, order_info, order_details
@@ -8,41 +8,37 @@ def home(request):
 
 def trackOrder(request):
     try:
-        print("(console.log) Updated")
         student_id = request.GET.get('student_id')
+
+        if student_id == "" or len(student_id) != 9:
+            return render(request, 'index.html', {'error_message': 'ID is not valid. Please try again.'})  
 
         orders = order_info.objects.filter(user_info_ID=student_id).values()
 
-        order_detail = order_details.objects.filter(order_details_ID=orders[0]['id']).values() #
-        
+        if not orders:
+            return render(request, 'index.html', {'error_message': 'No orders found for the provided student ID'}) 
+
+        order_detail = order_details.objects.filter(order_details_ID=orders[0]['id']).values()
+
         template = loader.get_template('trackOrder.html')
         content = {
             'orders': orders,
             'order_details': order_detail,
         }
-        
+
         return HttpResponse(template.render(content, request))
-    except user_info.DoesNotExist:
-        student = None
-        return render(request, "index.html", {'error_message': 'student id does not exist'})
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return redirect(request, 'index.html', {'error_message': 'An unexpected error occurred'})
 
 def aboutUs(request):
-    return render(request, "about_us.html")
+    return render(request, "aboutUs.html")
 
 def contactUs(request):
-    return render(request, "contact_us.html")
+    return render(request, "contactUs.html")
 
 def not_found(request):
     return render(request, '404.html', status=404)
-    
-def trackOrder(request):
-    return render(request, 'trackOrder.html')
-
-def aboutUs(request):
-    return render(request, 'aboutUs.html')
-
-def contactUs(request):
-    return render(request, 'contactUs.html')
 
 def adminTracker(request):
     return render(request, 'adminTracker.html')
