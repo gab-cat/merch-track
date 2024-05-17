aef1d4bbcbb64304bf139bbc97f2b6eb52bf3399
Reibanns <reivenlee@gmail.com>
2024-05-17 22:37:04 +0800

    Update: adminTracker

--- merchtrack/app/views.py
+++ merchtrack/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'adminTracker.html')
 
 def trackOrder(request):
     return render(request, 'trackOrder.html')
