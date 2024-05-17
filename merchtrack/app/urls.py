9233bfe0cd0d56053f43b1ff1fc40bfc3642937b
gab-cat <catimbanggabriel@gmail.com>
2024-05-17 20:46:16 +0800

    Update: Rebranch

--- merchtrack/app/urls.py
+++ merchtrack/app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path("trackOrder", views.home, name='trackOrder'),
     path("aboutUs", views.home, name='aboutUs'),
     path("contactUs", views.home, name='contactUs'),
+    re_path(r'^.*$', views.not_found, name='404'),
 ]
--- merchtrack/app/views.py
+++ merchtrack/app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def not_found(request):
+    return render(request, '404.html', status=404)
