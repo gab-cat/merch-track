a44569896eeb86ef7895f925ab70f16494188b63
gab-cat <catimbanggabriel@gmail.com>
2024-05-17 21:15:24 +0800

    Update: Static conf

--- merchtrack/app/urls.py
+++ merchtrack/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.views.static import serve
 from django.urls import path,re_path
 from . import views
 
@@ -6,5 +8,6 @@ urlpatterns = [
     path("trackOrder", views.home, name='trackOrder'),
     path("aboutUs", views.home, name='aboutUs'),
     path("contactUs", views.home, name='contactUs'),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     re_path(r'^.*$', views.not_found, name='404'),
 ]
