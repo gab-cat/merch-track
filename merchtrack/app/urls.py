2740d594d435c64c4baf531c6bc076b5e999d01b
kylaronquillo <kylasbronquillo@gmail.com>
2024-05-18 21:20:34 +0800

    Update aboutUs.html

--- merchtrack/app/urls.py
+++ merchtrack/app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'),
     path("trackOrder", views.home, name='trackOrder'),
-    path("aboutUs", views.home, name='aboutUs'),
+    path("aboutUs", views.aboutUs, name='aboutUs'),
     path("contactUs", views.home, name='contactUs'),
+    path("base", views.base, name='base'),
+    path("index", views.index, name='index'),
 ]
--- merchtrack/app/views.py
+++ merchtrack/app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
+    return render(request, 'home.html')
+
+def index(request):
     return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def aboutUs(request):
+    return render(request, 'aboutUs.html')
